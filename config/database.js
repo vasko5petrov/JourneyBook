4f465f013fe223333ea2ddaa2678c6739424fd6e
Vasko <vasko5petrov@gmail.com>
2017-11-17 13:35:38 +0200

    db ','

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 	// database: 'mongodb://localhost:27017/journeybook',
-	database: 'mongodb://vaskop:vasko5petrov@ds113136.mlab.com:13136/journeybook'
+	database: 'mongodb://vaskop:vasko5petrov@ds113136.mlab.com:13136/journeybook',
 	secret: 'secret'
 }
